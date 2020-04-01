5583953064851579c1cfb73ab8c8fef0ccb9cb80
NateSrkn <natesrkn@gmail.com>
2020-04-01 00:44:58 -0400

    Softened box-shadow

--- src/components/Button.js
+++ src/components/Button.js
@@ -14,17 +14,11 @@ const ButtonContainer = styled.button`
   background: transparent;
   border: none;
   border-radius: 5px;
-  box-shadow:
-  0 4.3px 9.3px rgba(0, 0, 0, 0.059),
-  0 12.8px 26.7px rgba(0, 0, 0, 0.085),
-  0 30.2px 64.5px rgba(0, 0, 0, 0.111),
-  0 82px 200px rgba(0, 0, 0, 0.17);
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.1);
   transition: ease .2s;
 
   &:hover {
-    box-shadow:
-      0 4.3px 9.3px rgba(0, 0, 0, 0.059),
-      0 12.8px 26.7px rgba(0, 0, 0, 0.085);
+    box-shadow: 0 4px 10px rgba(0, 0, 0, 0.1);
     transition: ease .2s;
   }
 `
--- src/components/TextInput.js
+++ src/components/TextInput.js
@@ -8,12 +8,7 @@ const TextContainer = styled.input`
   width: 100%;
   border: none;
   margin-bottom: 10px;
-  box-shadow:
-  0 4.3px 9.3px rgba(0, 0, 0, 0.059),
-  0 12.8px 26.7px rgba(0, 0, 0, 0.085),
-  0 30.2px 64.5px rgba(0, 0, 0, 0.111),
-  0 82px 200px rgba(0, 0, 0, 0.17)
-;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.1);
   border-radius: 5px;
 `
 
